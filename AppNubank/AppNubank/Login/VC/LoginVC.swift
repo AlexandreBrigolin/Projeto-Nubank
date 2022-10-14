7e2e3beebcc21465ba63491823330e854fb97e52
Barbara Brigolin <barbarabrigolin@hotmail.com>
2022-10-14 09:38:15 -0300

    adicionei a funcao de subir os elementos da tela quando o teclado subir

--- AppNubank/AppNubank/Login/VC/LoginVC.swift
+++ AppNubank/AppNubank/Login/VC/LoginVC.swift
@@ -25,10 +25,48 @@ class LoginVC: UIViewController {
         self.loginScreen?.configTextFieldDelegate(delegate: self)
         self.auth = Auth.auth()
         self.alert = Alert(controller: self)
+        self.configKeyoard()
     }
+    
+    
+    func configKeyoard(){
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardSubir(notification:)), name:UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardOriginal(notification:)), name:UIResponder.keyboardWillHideNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardSubir(notification:)), name:UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+
+    @objc func keyboardSubir(notification:Notification){
+        
+        print("O teclado mostrará: \(notification.name.rawValue)")
+        
+        view.frame.origin.y = -290
+    }
+    
+    @objc func keyboardOriginal(notification:Notification){
+        
+        print("O teclado mostrará: \(notification.name.rawValue)")
+        
+        view.frame.origin.y = 0
+        
+    }
+    
+    
 }
 
+
+
+
 extension LoginVC: UITextFieldDelegate {
+    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -48,14 +86,11 @@ extension LoginVC: UITextFieldDelegate {
 extension LoginVC: LoginScreenProtocol {
     
     func actionLoginButton() {
-//        let vc: HomeVC = HomeVC()
-//        self.navigationController?.pushViewController(vc, animated: true)
-      
-
+        
  guard let login = self.loginScreen else {return}
- 
- self.auth?.signIn(withEmail: login.getCPF(), password: login.getPassword(), completion: { user, error in
-     
+
+        self.auth?.signIn(withEmail: login.getCPF(), password: login.getPassword(), completion: { user, error in
+
      if error != nil {
          self.alert?.getAlert(title: "Atenção", message: "Dados incorretos, verifique seus dados")
      }else{
@@ -63,20 +98,16 @@ extension LoginVC: LoginScreenProtocol {
              self.alert?.getAlert(title: "Atenção", message: "Tivemos um problema inesperado, tente novamente mais tarde")
          }else{
              print("Login realizado com sucesso")
-             
+
              self.performSegue(withIdentifier: "RegisterVC", sender: nil)
          }
      }
- })
- 
-
-    }
+  })
+}
     
     func actionRegisterButton() {
         let vc: RegisterVC = RegisterVC()
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    
-    
-    
+  
 }
