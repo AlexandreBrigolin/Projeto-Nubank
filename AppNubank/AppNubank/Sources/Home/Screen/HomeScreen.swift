c9609e32e155ec628289d73cf100f239d72634c9
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2022-11-11 16:41:30 -0300

    organizacao das pastas source e resource e criacao da home

---
+++ AppNubank/AppNubank/Sources/Home/Screen/HomeScreen.swift
@@ -0,0 +1,14 @@
+//
+//  HomeScreen.swift
+//  AppNubank
+//
+//  Created by Alexandre Brigolin on 11/11/22.
+//
+
+import UIKit
+
+class HomeScreen: UIView {
+
+    
+
+}
--- AppNubank/AppNubank/Sources/Home/VC/HomeVC.swift
+++ AppNubank/AppNubank/Sources/Home/VC/HomeVC.swift
@@ -12,7 +12,7 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .blue
+        
     }
     
 
